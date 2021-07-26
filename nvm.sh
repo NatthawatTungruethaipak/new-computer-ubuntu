f5fc201859fb299a457d67db27018f7b1d0758e9
Natthawat Tungruethaipak <natthawat.tungruethaipak@mail.kmutt.ac.th>
2021-07-26 17:11:21 +0700

    change file to be export

--- nvm.sh
+++ nvm.sh
@@ -10,4 +10,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 ## Configure your shell's environment for nvm
 echo "export NVM_DIR=\"\$HOME/.nvm\"
 [ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"
-[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"" | tee -a ~/write_file.txt
+[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"" | tee -a ~/.profile ~/.bashrc
